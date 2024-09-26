85e8132767ef48e0d6ea5e3d773dbd4257c2d239
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-26 17:04:54 -0300

    ata

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
-// npm i express -- Para instalar o express
+// node.js instalar
+// vite framework
+// npm i express -- Para instalar o express, que é o api
 // npm init -y -- para fazer o package.json
 // incluir isso no package.json depois de license "dependencies": { "express": "^4.21.0"} e depois executá-lo para assim criar o package-lock.json
 // req = requisitar e res = resposta
 // quando acessar a url / vai ter a resposta Hello 
-// 
+// adicionar isso ,"dev": "node api.js" depois de test
+// rodar no terminar com npm run dev
 
 const express = require('express')
 const app = express();
@@ -15,6 +18,6 @@ app.get('/', (req,res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Exemplo de app sendo "escutado" na porta ${port}`)
 })
 
