215bba98435ba037224078299c8ff82b88fcfbd6
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:23:26 -0300

    atualizouu

--- api.js
+++ api.js
@@ -1,13 +1,59 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-
+app.use(express.json());
 const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send("Hello")
-})
+let personList = [];
+
+// Mostra todos
+app.get('/visualizar', (req, res) => {
+    res.send(personList);
+});
+
+// Mostra por ID
+app.get('/visualizar/:id', (req, res) => {
+    const { id } = req.params;
+    const person = personList[id];
+    if (person) {
+        res.send(person);
+    } else {
+        res.status(404).send('Usuário não encontrado');
+    }
+});
+
+// Novo usuário
+app.post('/cadastrar', (req, res) => {
+    const { id, name, age } = req.body;
+    personList.push({ id, name, age });
+    res.send(`Usuário recebido!\n\nID: ${id} \nNome do usuário: ${name}, \nAge: ${age}`);
+});
+
+// Atualizar por id
+app.put('/atualizar/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, age } = req.body;
+    const person = personList[id];
+    if (person) {
+        personList[id] = { name, age };
+        res.send(`Usuário atualizado!\nNovo nome: ${name} \nNova idade: ${age}`);
+    } else {
+        res.status(404).send('Usuário não encontrado');
+    }
+});
+
+// Deletar por id
+app.delete('/deletar/:id', (req, res) => {
+    const { id } = req.params;
+    if (personList[id]) {
+        personList.splice(id, 1);
+        res.send('Usuário deletado com sucesso');
+    } else {
+        res.status(404).send('Usuário não encontrado');
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Exemplo de app sendo "escutado" na porta ${port}`)
-})
+    console.log(`Exemplo de app sendo "escutado" na porta ${port}`);
+});
+
 
--- readme.md
+++ readme.md
@@ -1,11 +1,23 @@
 // node.js instalar
 // vite framework
 
+GIT:
+Para puxar o código do git para o vs code:
+cd dow + tab
+git commit + link do git 
+acessar o dow e pegar a pasta
+
+Para atualizar o código do git:
+certifique-se estar na pasta: git status
+para selecionar tudo o que está no vs code: git add .
+para modificar e adicionar no repositório: git commit -m
+
 Comandos básicos:
 // npm i express -- Para instalar o express, que é o api
 // npm init -y -- para fazer o package.json
 // incluir isso no package.json depois de license "dependencies": { "express": "^4.21.0"} e depois executá-lo para assim criar o package-lock.json
 // adicionar isso ,"dev": "node api.js" depois de test
+// ctrl + j abre o terminal
 
 Explicações do código:
 // req = requisitar e res = resposta
@@ -29,9 +41,19 @@ A P I
 Postman
 - Entrar e criar uma conta
 - Criar uma new collection
+- get
 - Clicar em new e colocar em http
 - Digitar http://localhost:3000
 - Vai aparecer o hello
+- post, json
+
+Nodemon
+// npm i nodemon - atualiza o server toda hora que salva o código
+// mudar depois de test no package.json "dev": "nodemon api.js"
 
 Função anônima ou arrow:
 - Com flexinha =>
+
+Método da pilha
+push - coloca na lista
+pop - tira o último da lista
